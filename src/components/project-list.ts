6452ef722556c5445e2bab70ab98e035bf621aea
SantoshChapagai <santoshchapagai100@gmail.com>
2023-08-13 14:30:38 +0300

    refactoring code with namespace exporting and importing

--- src/components/project-list.ts
+++ src/components/project-list.ts
@@ -1,4 +1,8 @@
 /// <reference path="base-component.ts" />
+/// <reference path="../decorators/autobind.ts" />
+/// <reference path="../model/project.ts" />
+/// <reference path="../model/drag-drop.ts" />
+/// <reference path="../state/project-state.ts" />
 
 namespace App {
   // ProjectList Class
